33d8e4822a3a32d0b5ced6a77bd972bc8099d8e7
Ashley <ashleykim@MacBook-Pro.local>
2019-05-01 15:06:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,14 +10,25 @@ let target;
 let ball;
 var level = 1;
 let wall;
+let targetSize = 50;
+
 
 function init() {
-    draw.rect();
+    cannon = new Rectangle(30, window.height-30, 50, 25, "black");
+    ball = new Ball(35, window.height -35, 10, "blue");
+
+    let targetX = Math.random() * canvas.width;
+    let targetY = Math.random() * canvas.height;
+    target = new Rectangle (targetX, targetY, targetSize, targetSize, "yellow");
+
+
+
     update();
 }
 
 function update() {
-
+    cannon.draw(c);
+    ball.draw(c);
     requestAnimationFrame(update);
 }
 
