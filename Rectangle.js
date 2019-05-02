e0703e1b19e7cf924ee38a8e229d2fafa17824b3
Ashley <ashleykim@MacBook-Pro.local>
2019-05-02 10:16:30 -0400

    Update Rectangle.js

--- Rectangle.js
+++ Rectangle.js
@@ -12,9 +12,12 @@ class Rectangle {
     draw(c){
         c.fillStyle = this.color;
         c.lineWidth = 5;
+        c.beginPath();
         c.rect(this.x, this.y, this.width, this.height);
+        c.closePath();
         c.stroke();
         c.fill();
+
 
     }
 
