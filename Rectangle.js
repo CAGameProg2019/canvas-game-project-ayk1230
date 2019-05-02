9d7d4cdc3198a857fcf06e43bbbace364dd353a9
Ashley <ashleykim@MacBook-Pro.local>
2019-05-02 10:04:59 -0400

    Update Rectangle.js

--- Rectangle.js
+++ Rectangle.js
@@ -1,16 +1,16 @@
 class Rectangle {
 
-    constructor(x, y, width, height, stroke) {
+    constructor(x, y, width, height, color) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.stroke = stroke;
+        this.color = color;
 
     }
 
     draw(c){
-        c.strokeStyle = this.stroke;
+        c.fillStyle = this.color;
         c.lineWidth = 5;
         c.rect(this.x, this.y, this.width, this.height);
         c.stroke();
