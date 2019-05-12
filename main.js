db98e93dffdb2e2e9eb4511c721bd6fc5ba57a9d
Ashley <ashleykim@MacBook-Pro.local>
2019-05-12 14:59:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,15 +11,23 @@ let ball;
 var level = 1;
 let wall;
 let targetSize = 50;
+var gravity = 5;
+var velocity = 10;
+var velocityx;
+var velocityy;
+var angle;
+var setAngle;
 var state = 'ready';
+
 
 
 function init() {
 
     mpos = new Vector(canvas.width/2, canvas.height/2);
 
+
     cannon = new Rectangle(0,0, 70, 30, "#0F7D16");
-    ball = new Ball(50, canvas.height - 35, 10, "#20DAFF");
+    ball = new Ball(3,5, 10, "#20DAFF");
 
     let targetX = canvas.width/2 + Math.random() * (canvas.width/2 - targetSize - 30);
     let targetY = Math.random() * (canvas.height - targetSize);
@@ -34,40 +42,88 @@ function init() {
 function update() {
     c.clearRect(0, 0, canvas.width, canvas.height);
 
+    wall.draw(c);
 
+    c.save();
+    c.translate(40, canvas.height -40);
+    angle = Math.atan2(-(canvas.height- 50 - mpos.y),(mpos.x-30));
+
+    if(angle<=0 && angle>=-90){
+        cannon.rotate(angle);
+        cannon.x = -13;
+        cannon.y = -9;
+        cannon.draw(c);
+        ball.draw(c);
+    }else if (angle <-90){
+        cannon.rotate(90);
+        cannon.x = -13;
+        cannon.y = -9;
+        cannon.draw(c);
+        ball.draw(c);
+    }else if (angle>0){
+        cannon.rotate(0);
+        cannon.x = -13;
+        cannon.y = -9;
+        cannon.draw(c);
+        ball.draw(c);
+    }
+
+    c.restore();
+
+    if(mouseDown){
+        state = "shooting";
+        setAngle = angle;
+    }
+    if(state == 'shooting'){
+
+        velocityx = velocity*(Math.cos(setAngle));
+        velocityy = velocity*(Math.sin(setAngle));
+
+        ball.x += velocityx;
+        ball.y = ball.y - velocityy + gravity;
+
+        ball.draw(c);
+    }
+
+    c.lineWidth = 15;
+    c.font = "20px Arial";
+    c.fillStyle = "black";
+    c.fillText("Level: "+ level, 40, 50);
 
     let crash = ball.intersects(target);
     if(crash){
         target.color = "red";
         target.draw(c);
-        level ++;
+        velocity = 0;
+        level+=1;
+
+        setTimeout();
     }else{
         target.draw(c);
     }
 
+    // if(keyPress.s){
+    //
+    // }
 
-    wall.draw(c);
-
-    c.save();
-    c.translate(40, canvas.height -40);
-    var angle = Math.atan2(-(canvas.height- 50 - mpos.y),(mpos.x-30));
-
-    cannon.rotate(angle);
-    cannon.x = -13;
-    cannon.y = -9;
-    cannon.draw(c);
-
-    c.restore();
-
-    ball.draw(c);
-
-    c.lineWidth = 15;
-    c.font = "20px Arial";
-    c.fillStyle = "black";
-    c.fillText("level: "+ level, 20, 50);
 
     requestAnimationFrame(update);
 }
+
+setTimeout(function(){
+    window.location.reload();
+},6000);
+
+var mouseDown = false;
+var mouseUp = false;
+
+// function getMousePos(canvas, event){
+//     var rect = canvas.getBoundingCLientRect();
+//     return {
+//         x: event.clientX,
+//         y: event.clientY
+//     };
+// }
 
 window.addEventListener('load', function() {
     init();
@@ -83,5 +139,16 @@ window.addEventListener('load', function() {
         mpos.print();
 
     });
+
+    window.addEventListener('mousedown', function(event){
+        mouseDown = true;
+        mouseUp = false;
+    });
+
+    window.addEventListener('mouseup',function(event){
+        mouseDown = false;
+        mouseUp = true;
+    })
+
 
 });
